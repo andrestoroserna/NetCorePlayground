8b024ec4831ebf81bd44592be704d1a298199879
Andrés Felipe Toro Serna <andres.toro.serna@gmail.com>
2020-05-20 08:50:15 -0500

    Test

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NetCorePlayground.DataAccess;
-using UnitOfWork;
+using UnitOfWork; 
 
 namespace WebApi
 {
